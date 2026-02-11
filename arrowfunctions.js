418e39ce303174b7c525923894ba2075ead07064
bindupriya-03 <bindugajendrula09@gmail.com>
2026-02-11 21:53:25 +0530

    Add files via upload

---
+++ arrowfunctions.js
@@ -0,0 +1,16 @@
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+
+function modifyArray(nums) {
+    return nums.map(function(num) {
+        if (num % 2 === 0) {
+            return num * 2;
+        } else {
+            return num * 3;
+        }
+    });
+}
